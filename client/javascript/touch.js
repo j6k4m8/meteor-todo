874abf6f0d1293ef785382b18a7c7816aed7dcda
JM <j6k4m8@gmail.com>
2015-02-26 19:36:41 -0500

    Remove touch (for now)

--- client/javascript/touch.js
+++ client/javascript/touch.js
@@ -1,8 +1,8 @@
 Template.show_task_row.gestures({
-   'swiperight .task-row': function() {
+   /*'swiperight .task-row': function() {
         Meteor.call('completeTask', this.data._id);
     },
     'swipeleft .task-row': function() {
         Meteor.call('setDueDate', this.data._id, moment().add(1, 'd').toDate());
-    } 
+    }*/ 
 });
